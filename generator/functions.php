9eb51b7573e1e17e7aad6ec1a48b3d65585a2da6
NikulinaTanya <nika3621210@yandex.ru>
2017-06-02 07:57:36 +0700

    edit

--- generator/functions.php
+++ generator/functions.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
 error_reporting(E_ERROR);
+if(!isset($_SESSION['user_login'])){
+    $_SESSION['user_login'] = '';
+}
 
 function MakeAmount(){
     $array = unserialize(file_get_contents($_SERVER['DOCUMENT_ROOT']."/generator/db/amount.txt"));
