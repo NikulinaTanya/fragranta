86948b774dae39268dbabbfa6fd801d008432bff
NikulinaTanya <nika3621210@yandex.ru>
2017-06-02 07:51:27 +0700

    edit

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php  include($_SERVER['DOCUMENT_ROOT']."/generator/functions.php"); session_start(); ?>
+<?php
+ini_set('display_errors',0);
+error_reporting(E_ALL);
+include($_SERVER['DOCUMENT_ROOT']."/generator/functions.php"); session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
